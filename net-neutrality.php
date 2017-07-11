92ac0ef23cba971b2ee83fdacc69d070614e8e34
Caspar Hübinger <caspar@glueckpress.com>
2017-07-11 09:47:43 +0200

    various enhancements
    **plugin header:**
    - added semantic versioning
    - added license URI
    - minor formatting
    
    **after header:**
    - added line to prevent direct access
    
    **added new method:** `get_version()`
    
    `init()`:
    - minor formatting
    
    `__construct()`:
    - changed hook to `wp_enqueue_scripts` instead of `wp_footer`
    
    `admin_menu()`:
    - added i18n context
    
    `get_option()`:
    - bail with boolean
    - changed operator from not equal to not identical
    
    `admin_init()`:
    - removed registering of unused stylesheet
    
    `show_ribbon()`:
    - bail with boolean
    - changed operator from equal to identical
    
    `enqueue_style()`:
    - bail with boolean
    - added i18n comments
    - added `button` to allowed HTML
    - added plugin version to enqueued scripts/stylesheets
    - minor editorial edits of content output
    
    `ribbon()`:
    - bail with boolean
    - minor editorial edit of content output

--- net-neutrality.php
+++ net-neutrality.php
@@ -3,13 +3,15 @@
 /*
  * Plugin Name: Net Neutrality by WordPress.com
  * Description: Slow your site down to fight for net neutrality.
- * Author: Automattic
- * Version: 0.01
- * Author URI: http://wordpress.com
- * License: GPL2+
+ * Author:      Automattic
+ * Version:     0.0.1
+ * Author URI:  http://wordpress.com
+ * License:     GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: net-neutrality-wpcom
  */
 
+defined( 'ABSPATH' ) or die( 'No direct access here.' );
 
 // TODO: Better attribution
 // Based off the Net_Neutrality plugin by DLLH
@@ -25,8 +27,9 @@ class Net_Neutrality {
 
 		static $instance = false;
 
-		if ( $instance )
+		if ( $instance ) {
 			return $instance;
+		}
 
 		$instance = new Net_Neutrality;
 
@@ -35,18 +38,25 @@ class Net_Neutrality {
 
 	function __construct() {
 
-		add_action( 'wp_footer', array( $this, 'ribbon' ) );
+		// TODO: Settings for admins only?
 		add_action( 'admin_init', array( $this, 'admin_init' ) );
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
 
-		// TODO: Probably load this in the header to minimize flashing content?
-		add_action( 'wp_footer', array( $this, 'enqueue_style' ), 11 );
+		add_action( 'wp_footer', array( $this, 'ribbon' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_style' ) );
+	}
+
+	function get_version() {
+
+		$file_data = get_file_data( __FILE__, array( 'v' => 'Version' ) );
+
+		return $file_data['v'];
 	}
 
 	function admin_menu() {
 		$this->screen_id = add_options_page(
-			'Fight for Net Neutrality',
-			'Fight for Net Neutrality',
+			_x( 'Fight for Net Neutrality', 'page title', 'net-neutrality-wpcom' ),
+			_x( 'Fight for Net Neutrality', 'menu title', 'net-neutrality-wpcom' ),
 			'manage_options',
 			'net-neutrality',
 			array( $this, 'options' )
@@ -55,7 +65,7 @@ class Net_Neutrality {
 
 	function get_option( $key ) {
 		if ( ! isset( $this->defaults[ $key ] ) ) {
-			return;
+			return false;
 		}
 
 		$options = get_option( 'net_neutrality_options' );
@@ -75,7 +85,7 @@ class Net_Neutrality {
 		$input = array_intersect_key( $input, $this->defaults );
 		$input = wp_parse_args( $input, $this->defaults );
 
-		if ( 'on' != $input['enabled'] ) {
+		if ( 'on' !== $input['enabled'] ) {
 			$input['enabled'] = 'off';
 		}
 
@@ -83,40 +93,44 @@ class Net_Neutrality {
 	}
 
 	function admin_init() {
-		wp_register_style( 'net-neutrality', plugins_url( 'net-neutrality/net-neutrality.css', __FILE__ ) );
 		register_setting( 'net_neutrality_options', 'net_neutrality_options', array( $this, 'validate' ) );
 	}
 
 	function show_ribbon() {
 
 		if ( time() > strtotime( '2017-07-12 23:59:59' ) ) {
-			return;
+			return false;
 		}
 
-		return 'on' == $this->get_option( 'enabled' );
+		return 'on' === $this->get_option( 'enabled', false );
 	}
 
 	function enqueue_style() {
+
 		if ( ! $this->show_ribbon() ) {
-			return;
+			return false;
 		}
 
+		$v = $this->get_version();
+
 		$yep_still_loading = wp_kses(
-			esc_html__( 'Yep... <em>still</em> loading...', 'net-neutrality-wpcom' ),
+			/* translators: use only <em>, other HTML tags will be stripped */
+			esc_html__( 'Yep&nbsp;… <em>still</em> loading&nbsp;…', 'net-neutrality-wpcom' ),
 			array( 'em' => array() )
 		);
 
 		$will_happen_string = wp_kses(
+			/* translators: use only <br> and <button id>, other HTML tags will be stripped */
 			esc_html__( 'This is what will happen without Net Neutrality. <br><button id="net-neutrality-stop">Make it stop!</button>', 'net-neutrality-wpcom' ),
-			array( 'br' => array(), 'strong' => array() )
+			array( 'br' => array(), 'button' => array( 'id' => array() ) )
 		);
 
-		wp_enqueue_style( 'net-neutrality', plugins_url( 'net-neutrality.css', __FILE__ ), array(), '20140904' );
-		wp_register_script( 'net-neutrality-js', plugins_url( 'net-neutrality.js', __FILE__ ), array(), '20140904' );
+		wp_enqueue_style( 'net-neutrality', plugins_url( 'net-neutrality.css', __FILE__ ), array(), $v );
+		wp_register_script( 'net-neutrality-js', plugins_url( 'net-neutrality.js', __FILE__ ), array( 'jquery' ), $v, true );
 		wp_localize_script( 'net-neutrality-js', 'netNeutrality', array(
 			'strings' => array(
-				'loading'         => esc_html__( 'Loading...', 'net-neutrality-wpcom' ),
-				'stillLoading'    => esc_html__( 'Still loading...', 'net-neutrality-wpcom' ),
+				'loading'         => esc_html__( 'Loading&nbsp;…', 'net-neutrality-wpcom' ),
+				'stillLoading'    => esc_html__( 'Still loading&nbsp;…', 'net-neutrality-wpcom' ),
 				'yepStillLoading' => $yep_still_loading,
 				'willHappen'      => $will_happen_string,
 			)
@@ -125,14 +139,15 @@ class Net_Neutrality {
 	}
 
 	function ribbon() {
+
 		if ( ! $this->show_ribbon() ) {
-			return;
+			return false;
 		}
 
 		?>
 		<div id="net-neutrality-overlay" style="display: none;">
 			<div id="net-neutality-overlay-content">
-				<p><?php esc_html_e( "Isn't this frustrating?", 'net-neutrality-wpcom' ); ?></p>
+				<p><?php esc_html_e( "Isn’t this frustrating?", 'net-neutrality-wpcom' ); ?></p>
 				<p><?php esc_html_e( 'Help keep the internet free of slow lanes by supporting net neutrality.', 'net-neutrality-wpcom' ); ?></p>
 				<p>
 					<a id="net-neutrality-overlay-action" href="http://battleforthenet.com" target="_blank">
